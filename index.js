baa9f2640825007c65e51a32a2cb710d7a0bd208
donglyc <silencesoon@vip.qq.com>
2021-04-09 15:26:17 +0800

    fix:v2

--- index.js
+++ index.js
@@ -1,207 +1,82 @@
 import QueryString from 'query-string';
-import React, { useRef } from "react";
+import React, { useRef } from 'react';
 import { View, ActivityIndicator } from 'react-native';
 import { WebView } from 'react-native-webview';
 import { NativeModules } from 'react-native';
 
 const { AuthingRnSdk } = NativeModules;
 
-const Guard = ({
-    userPoolId,
-    options = {},
-    onLogin = (userInfo) => { },
-    onLoginError = error => { },
-    onSocialLoad = socialList => { },
-    onSocialUnload = error => { },
-    onRegister = userInfo => { },
-    onRegisterError = error => { },
-    onEmailSent = params => { },
-    onEmailSentError = error => { },
-    onResetPassword = params => { },
-    onResetPasswordError = error => { },
-}) => {
-    const guardRef = useRef(null)
-    // 用户池信息
-    const injectGuardOptions = `
-  window.ReactNativeWebView.GuardConfig = {
-    userPoolId: '${userPoolId}',
-    options: ${JSON.stringify(options)}
-  }
-  `
+const version = '2.3.5';
 
-    // 写死的配置项，不管开发者怎么设置，不然会出问题
-    options.hideClose = true
-    options.isNative = true
-    options.isSSO = false
+const Guard = props => {
+	const { appId = '5d70d0e991fdd597019df70d', options = {} } = props;
 
-    function sendNativeLoginResponse(loginType, success, data) {
-        guardRef.current.injectJavaScript(`
-window.ReactNativeWebView.nativeLoginResponse = {
-    loginType: '${loginType}',
-    success: ${success},
-    data: ${JSON.stringify(data)}
-}
-`)
-    }
+	const guardRef = useRef(null);
 
-    let guardSrc = 'http://cdn.jsdelivr.net/npm/@authing/guard/dist/Guard.umd.min.js'
-    // let guardSrc = 'http://192.168.2.16:8082/dist/Guard.umd.min.js'
+	// 写死的配置项，不管开发者怎么设置，不然会出问题
+	options.hideClose = true;
+	options.isNative = true;
+	options.isSSO = false;
 
-    let html = `
-<!DOCTYPE html>
-<html lang="zh-cn">
-<head>
-  <meta charset="utf-8" />
-  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-  <meta name="viewport" content="width=device-width,initial-scale=1.0" />
-  <link rel="icon" href="https://fe-static.authing.cn/dist/favicon.png" />
-  <title>Authing SSO</title>
-  <script src=${JSON.stringify(guardSrc)}></script>
-</head>
-<body>
-  <noscript>
-    <strong>We're sorry but sso doesn't work properly without JavaScript enabled.
-      Please enable it to continue.</strong>
-  </noscript>
-  <div class="authing-loading-circle screen-center" id="loading"></div>
-  <div id="app"></div>
-</body>
-<script>
-const guard = new Guard(${JSON.stringify(userPoolId)}, ${JSON.stringify(options)});
-</script>
-</html>
-`
+	let staticHtmlHead = `
+	<link href='https://cdn.jsdelivr.net/npm/@authing/native-js-ui-components@${version}/lib/index.min.css' rel='stylesheet' ></link>`;
 
-    async function loginByAlipay() {
-        let domain = ""
-        if (options.host && options.host.oauth) {
-            domain = options.host.oauth.replace('/graphql', '')
-        } else {
-            domain = "https://oauth.authing.cn"
-        }
-        const getAuthInfoUrl = `${domain}/oauth/alipaymobile/authinfo/${userPoolId}`
-        const getUserInfoUrl = `${domain}/oauth/alipaymobile/redirect/${userPoolId}`
-        let response = await fetch(getAuthInfoUrl)
-        let responseJson = await response.json()
-        if (responseJson.code === 200) {
-            const authInfo = responseJson.data
-            try {
-                let response = await AuthingRnSdk.authWithInfo(authInfo);
-                // console.log(response)
-                let { resultStatus, result, memo } = response;
-                let { success, result_code, auth_code, user_id } = QueryString.parse(result);
-                // 请求处理成功
-                if (resultStatus == 9000 && result_code == 200) {
-                    response = await fetch(getUserInfoUrl, {
-                        method: 'POST',
-                        headers: {
-                            'Content-Type': 'application/json',
-                        },
-                        body: JSON.stringify({
-                            auth_code,
-                            result_code,
-                            user_id
-                        })
-                    })
-                    responseJson = await response.json()
-                    const success = responseJson.code === 200
-                    const data = responseJson.data
-                    sendNativeLoginResponse("alipaymobile", success, data)
-                } else {
-                    let errmsg = ""
-                    if (resultStatus == 4000) {
-                        errmsg = "系统异常"
-                    } else if (resultStatus == 6001) {
-                        errmsg = "中途取消"
-                    } else if (resultStatus == 6002) {
-                        errmsg = "网络连接出错"
-                    }
-                    sendNativeLoginResponse("alipaymobile", false, errmsg)
-                }
-            } catch (error) {
-                console.error(error);
-            }
-        }
-    }
+	let staticScript = `var head= document.getElementsByTagName('head')[0];
+	var script= document.createElement('script');
+	script.type= 'text/javascript';
+	script.onreadystatechange= function () {}
+	script.onload= function(){
+		var guard = new AuthingNativeJsUIComponents.AuthingGuard('${appId}')
+		// 事件监听
+		guard.on('load', (authClient) => console.log(authClient))
+		// 发送事件
+		Object.entries(AuthingNativeJsUIComponents.GuardEventsCamelToKebabMap).forEach(([evt,e]) => {
+			guard.on(e ,(...rest) => {
+				window.ReactNativeWebView.postMessage(JSON.stringify({
+					evtName: evt,
+					params: rest
+				}))
+			})
+		})
+	 }
+	script.src= 'https://cdn.jsdelivr.net/npm/@authing/native-js-ui-components@${version}';
+	head.appendChild(script)`;
 
-    return (
-        <View style={{ flex: 1, flexDirection: 'column' }}>
-            <WebView
-                ref={guardRef}
-                source={{
-                    // uri: domain
-                    html: html
-                }}
-                originWhitelist={['*']}
-                renderLoading={
-                    <ActivityIndicator
-                        color="#009b88"
-                        size="large"
-                        style={{
-                            flex: 1,
-                            justifyContent: 'center'
-                        }}
-                    />
-                }
-                injectedJavaScript={injectGuardOptions}
-                onMessage={e => {
-                    const eventDetail = JSON.parse(e.nativeEvent.data)
-                    let { eventName, params } = eventDetail;
-
-                    // 错误信息只不显示 trackback 等无用信息
-                    if (params.message && params.message.message) {
-                        params = params.message
-                    }
-                    // console.log(eventName, params)
-                    switch (eventName) {
-                        case "login":
-                            onLogin(params)
-                            break
-                        case "login-error":
-                            onLoginError(params)
-                            break
-                        case "social-load":
-                            onSocialLoad(params)
-                            break
-                        case "social-unload":
-                            onSocialUnload(params)
-                            break
-                        case "register":
-                            onRegister(params)
-                            break
-                        case "register-error":
-                            onRegisterError(params)
-                            break
-                        case "email-sent":
-                            onEmailSent(params)
-                            break
-                        case "email-sent-error":
-                            onEmailSentError(params)
-                            break
-                        case "reset-password":
-                            onResetPassword(params)
-                            break
-                        case "reset-password-error":
-                            onResetPasswordError(params)
-                            break
-                        case "form-closed":
-                            onGuardClosed()
-                            break
-                        case "start-native-login":
-                            if (params === "alipaymobile") {
-                                loginByAlipay()
-                            }
-                            break
-                    }
-                }}
-                onContentProcessDidTerminate={() => guardRef.current.reload()}
-            />
-        </View>
-
-    );
+	const injectGuardOptions = `
+	window.ReactNativeWebView.html={
+		head:\`${staticHtmlHead}\`,
+		$script:\`${staticScript}\`
+	}
+`;
+	return (
+		<View style={{ flex: 1, flexDirection: 'column' }}>
+			<WebView
+				ref={guardRef}
+				source={{
+					uri: 'https://console.authing.cn/react-native/webview'
+				}}
+				originWhitelist={['*']}
+				renderLoading={
+					<ActivityIndicator
+						color="#009b88"
+						size="large"
+						style={{
+							flex: 1,
+							justifyContent: 'center'
+						}}
+					/>
+				}
+				injectedJavaScript={injectGuardOptions}
+				onMessage={e => {
+					const eventDetail = JSON.parse(e.nativeEvent.data);
+					let { evtName, params } = eventDetail;
+					props[evtName] && props[evtName](params);
+				}}
+				onContentProcessDidTerminate={() => guardRef.current.reload()}
+			/>
+		</View>
+	);
 };
-
-
 module.exports = {
-    Guard
-}
+	Guard
+};
